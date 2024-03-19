ffd77992b3f576d739c8adf8a86857811c75a1b7
SaltedFish <1720466861@qq.com>
2024-03-19 15:41:18 +0800

    改进AOP，将日志aop移入model模块，并新增权限认证aop

---
+++ woj-backend-model/src/main/java/com/woj/model/aop/AuthInterceptor.java
@@ -0,0 +1,56 @@
+package com.woj.model.aop;
+
+import com.woj.model.annotation.AuthCheck;
+import com.woj.common.common.ErrorCode;
+import com.woj.common.exception.BusinessException;
+import com.woj.model.entity.User;
+import com.woj.model.enums.UserRoleEnum;
+import org.apache.commons.lang3.StringUtils;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static com.woj.common.constant.UserConstant.USER_LOGIN_STATE;
+
+@Aspect
+@Component
+public class AuthInterceptor {
+    @Around("@annotation(authCheck)")
+    public Object doInterceptor(ProceedingJoinPoint joinPoint, AuthCheck authCheck) throws Throwable {
+        String mustRole = authCheck.mustRole();
+        RequestAttributes requestAttributes = RequestContextHolder.currentRequestAttributes();
+        HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+        // 当前登录用户
+        Object userObj = request.getSession().getAttribute(USER_LOGIN_STATE);
+        User currentUser = (User) userObj;
+        if (currentUser == null || currentUser.getId() == null) {
+            throw new BusinessException(ErrorCode.NOT_LOGIN_ERROR);
+        }
+        // 必须有该权限才通过
+        if (StringUtils.isNotBlank(mustRole)) {
+            UserRoleEnum mustUserRoleEnum = UserRoleEnum.getEnumByValue(mustRole);
+            if (mustUserRoleEnum == null) {
+                throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+            }
+            String userRole = currentUser.getUserRole();
+            // 如果被封号，直接拒绝
+            if (UserRoleEnum.BAN.equals(mustUserRoleEnum)) {
+                throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+            }
+            // 必须有管理员权限
+            if (UserRoleEnum.ADMIN.equals(mustUserRoleEnum)) {
+                if (!mustRole.equals(userRole)) {
+                    throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+                }
+            }
+        }
+        // 通过权限校验，放行
+        return joinPoint.proceed();
+    }
+}
