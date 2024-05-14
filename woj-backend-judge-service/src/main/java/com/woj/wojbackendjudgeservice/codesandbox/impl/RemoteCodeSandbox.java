63c9382cdf0972340c4c8f4edea79e8cca0485c4
SaltedFish <1720466861@qq.com>
2024-05-14 13:48:13 +0800

    使用api签名认证机制访问代码沙箱api接口

--- woj-backend-judge-service/src/main/java/com/woj/wojbackendjudgeservice/codesandbox/impl/RemoteCodeSandbox.java
+++ woj-backend-judge-service/src/main/java/com/woj/wojbackendjudgeservice/codesandbox/impl/RemoteCodeSandbox.java
@@ -7,15 +7,45 @@ import com.woj.model.enums.QuestionSubmitStatusEnum;
 import com.woj.model.judge.ExecuteCodeRequest;
 import com.woj.model.judge.ExecuteCodeResponse;
 import com.woj.wojbackendjudgeservice.codesandbox.CodeSandbox;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.UUID;
 
 public class RemoteCodeSandbox implements CodeSandbox {
+    private static final String app_key="first";
+    private static final String app_secret="wangheng";
 
+    private static final String HMAC_SHA256_ALGORITHM = "HmacSHA256";
     @Override
     public ExecuteCodeResponse doExecute(ExecuteCodeRequest executeCodeRequest) {
+        String json = JSONUtil.toJsonStr(executeCodeRequest);
+        SecretKeySpec secretKeySpec = new SecretKeySpec(app_secret.getBytes(StandardCharsets.UTF_8),HMAC_SHA256_ALGORITHM);
+        Mac mac = null;
+        String sign=null;
+        long timestamp=System.currentTimeMillis()/1000;
+        String nonce= UUID.randomUUID().toString(); // TODO 生成随机字符串
+        try {
+            mac = Mac.getInstance(HMAC_SHA256_ALGORITHM);
+            mac.init(secretKeySpec);
+            byte[] hmacData = mac.doFinal((json+timestamp+nonce).getBytes(StandardCharsets.UTF_8));
+            sign= Base64.getEncoder().encodeToString(hmacData);
+        } catch (NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
         System.out.println("remote");
         HttpResponse response = HttpUtil.createPost("http://localhost:8090/codesandbox/executeCode")
-                .header("auth","secret")
-                .body(JSONUtil.toJsonStr(executeCodeRequest))
+                .header("appKey",app_key)
+                .header("sign",sign)
+                .header("timestamp", String.valueOf(timestamp))
+                .header("nonce",nonce)
+                .body(json)
                 .execute(false);
         if(response.getStatus()!=200){
             System.out.println("http状态码:"+response.getStatus());
